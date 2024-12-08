226a767c9d5f8ee44c55bf52764c4435f54a4ab0
Miguel Abache <miiguel.abache@gmail.com>
2024-12-08 18:10:38 -0400

    Config mysql

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(process.env.APP_PORT);
+  console.log(`App running on port ${process.env.APP_PORT}`);
 }
 bootstrap();
